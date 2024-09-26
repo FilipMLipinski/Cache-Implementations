5e3fe051d2341b618858a1cde43fdde7b860c881
FilipMLipinski <72980477+FilipMLipinski@users.noreply.github.com>
2024-09-26 15:30:29 +0100

    added LRU and tests

--- LRUCache.java
+++ LRUCache.java
@@ -1,21 +1,75 @@
-class LRUCache{
-    private Node firstNode;
-    private Node lastNode;
-    private HashMap<Integer, Node> map;
-    private Integer size;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
-    public LRUCache(Integer _size){
-        size = _size;
+class LRUCache{
+    private Node head;
+    private Node tail;
+    private HashMap<Integer, Node> map = new HashMap<Integer, Node>();
+    private Integer maxsize;
+    private Integer cursize = 0;
+
+    private void listAddFront(Node n){
+        Node first = head.next;
+        head.next = n;
+        n.next = first;
+        n.prev = head;
+        first.prev = n;
+        cursize++;
+    }
+
+    private void listRemove(Node n){
+        Node pre = n.prev;
+        Node nex = n.next;
+        pre.next = nex;
+        nex.prev = pre;
+        cursize--;
+    }
+
+    private Integer listPeekBack(){
+        return tail.prev.val;
+    }
+
+    public LRUCache(Integer _maxsize){
+        maxsize = _maxsize;
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.prev = head;
     }
 
     public boolean query(Integer q){
-        return true;
+        if(maxsize==0){
+            return false;
+        }
+        Node cachedNode = map.get(q);
+        if(cachedNode==null){
+            if(cursize==maxsize){
+                Integer outgoing = listPeekBack();
+                listRemove(map.get(outgoing));
+                map.remove(outgoing);
+            }
+            Node n = new Node();
+            n.val = q;
+            listAddFront(n);
+            map.put(q, n);
+            return false;
+        }
+        else{
+            listRemove(cachedNode);
+            listAddFront(cachedNode);
+            return true;
+        }
     }
 
     public ArrayList<Integer> getCache(){
         ArrayList<Integer> ret = new ArrayList<>();
+        Node currnode = head.next;
+        while(currnode!=tail){
+            ret.add(currnode.val);
+            currnode = currnode.next;
+        }
+        Collections.sort(ret);
         return ret;
     }
-
-
 }
--- Tests.java
+++ Tests.java
@@ -31,6 +31,42 @@ public class Tests{
         return true;
     }
 
+    private static boolean test1LRU(){
+        LRUCache cache = new LRUCache(3);
+        if(!(cache.query(1)==false)){
+            System.out.println("inserting into empty and cache hit");
+            return false;
+        }
+        if(!(cache.query(1)==true)){
+            System.out.println("expecting cache hit");
+            return false;
+        }
+        if(!(cache.query(2)==false)){
+            System.out.println("expecting cache miss");
+            return false;
+        }
+        if(!(cache.query(3)==false)){
+            System.out.println("expecting cache miss");
+            return false;
+        }
+        if(!(cache.query(1)==true)){
+            System.out.println("expecting cache hit");
+            return false;
+        }
+        if(!(cache.query(4)==false)){
+            System.out.println("expecting cache miss");
+            return false;
+        }
+        ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(1,3,4));
+        if(!(cache.getCache()).equals(expected)){
+            System.out.print(cache.getCache());
+            System.out.print(" does not match expected: ");
+            System.out.println(expected);
+            return false;
+        }
+        return true;
+    }
+
     public static void main(String args[]){
         System.out.println("Testing the LFU cache");
         if(test1LFU()){
@@ -40,5 +76,8 @@ public class Tests{
             System.out.println("test 2 passed");
         }
         System.out.println("Testing the LRU cache");
+        if(test1LRU()){
+            System.out.println("test 1 passed");
+        }
     }
 }
